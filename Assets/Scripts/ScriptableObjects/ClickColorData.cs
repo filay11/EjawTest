d476d5fe0c101d5bff08bb6b3626b527d8f4e4bf
BLACKBAKERY\Professional <filay11@gmail.com>
2020-10-26 11:22:30 +0300

    from test

--- Assets/Scripts/ScriptableObjects/ClickColorData.cs
+++ Assets/Scripts/ScriptableObjects/ClickColorData.cs
@@ -3,6 +3,7 @@
 [CreateAssetMenu(fileName = "ColorData.asset", menuName = "ColorData")]
 public class ClickColorData : ScriptableObject
 {
+    // changes from test
     public string ObjectType;
     public int MinClicksCount;
     public int MaxClicksCount;
